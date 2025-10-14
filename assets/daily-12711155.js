09dd9ad226600a1b78efa96422f22f8f5183af49
滑稽大帝 <2785907742@qq.com>
2025-10-14 22:41:40 +0800

    again

---
+++ assets/daily-12711155.js
@@ -0,0 +1,3 @@
+const a=[{name:"Matrix2DRecord",from:"daily",slug:"/src/articles/daily/Matrix2DRecord.md",path:"/page",frontmatter:{title:"变换矩阵学习",date:"2025-10-14",tags:["daily","math"],excerpt:`\r
+记录二维下对图形变换的系列矩阵\r
+`}}];export{a as default};
